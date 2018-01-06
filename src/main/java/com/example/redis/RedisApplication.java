a0be26eec0c3007e6ed46a33096f11da5f9d0dc5
Jack.zhang <zhenhua.zhang@alo7.com>
2018-01-06 17:42:52 +0800

    customer redis configuration

--- src/main/java/com/example/redis/RedisApplication.java
+++ src/main/java/com/example/redis/RedisApplication.java
@@ -2,10 +2,13 @@ package com.example.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableCaching
 public class RedisApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RedisApplication.class, args);
 	}
--- src/main/java/com/example/redis/controller/UserInfoController.java
+++ src/main/java/com/example/redis/controller/UserInfoController.java
@@ -3,6 +3,7 @@ package com.example.redis.controller;
 import com.example.redis.dao.UserInfoEntity;
 import com.example.redis.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,7 +18,9 @@ public class UserInfoController {
 
     @RequestMapping("users")
     @ResponseBody
+    @Cacheable(value = "user",cacheManager = "redisCacheManagerString")
     public List<UserInfoEntity> findAll(){
+        System.out.println("user........");
         return userInfoService.findAll();
     }
 
